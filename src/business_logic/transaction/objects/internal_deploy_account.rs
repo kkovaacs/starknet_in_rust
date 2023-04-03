59cb49b0e70cb53251e3824edc5430c34886915f
Krisztian Kovacs <kkovaacs@gmail.com>
2023-04-03 15:01:51 +0200

    tmp: transaction execution should not require clonable state

--- src/business_logic/transaction/objects/internal_deploy_account.rs
+++ src/business_logic/transaction/objects/internal_deploy_account.rs
@@ -98,7 +98,7 @@ impl InternalDeployAccount {
         general_config: &StarknetGeneralConfig,
     ) -> Result<TransactionExecutionInfo, TransactionError>
     where
-        S: Clone + State + StateReader,
+        S: State + StateReader,
     {
         let tx_info = self.apply(state, general_config)?;
 
@@ -194,10 +194,7 @@ impl InternalDeployAccount {
         }
     }
 
-    fn handle_nonce<S: State + StateReader + Clone>(
-        &self,
-        state: &mut S,
-    ) -> Result<(), TransactionError> {
+    fn handle_nonce<S: State + StateReader>(&self, state: &mut S) -> Result<(), TransactionError> {
         if self.version == 0 {
             return Ok(());
         }
@@ -317,7 +314,7 @@ impl InternalDeployAccount {
         general_config: &StarknetGeneralConfig,
     ) -> Result<FeeInfo, TransactionError>
     where
-        S: Clone + State + StateReader,
+        S: State + StateReader,
     {
         if self.max_fee.is_zero() {
             return Ok((None, 0));
--- src/business_logic/transaction/objects/internal_invoke_function.rs
+++ src/business_logic/transaction/objects/internal_invoke_function.rs
@@ -184,7 +184,7 @@ impl InternalInvokeFunction {
         general_config: &StarknetGeneralConfig,
     ) -> Result<TransactionExecutionInfo, TransactionError>
     where
-        T: State + StateReader + Clone,
+        T: State + StateReader,
     {
         let mut resources_manager = ExecutionResourcesManager::default();
 
@@ -219,7 +219,7 @@ impl InternalInvokeFunction {
         general_config: &StarknetGeneralConfig,
     ) -> Result<FeeInfo, TransactionError>
     where
-        S: Clone + State + StateReader,
+        S: State + StateReader,
     {
         if self.max_fee.is_zero() {
             return Ok((None, 0));
@@ -240,7 +240,7 @@ impl InternalInvokeFunction {
 
     /// Calculates actual fee used by the transaction using the execution info returned by apply(),
     /// then updates the transaction execution info with the data of the fee.
-    pub fn execute<S: State + StateReader + Clone>(
+    pub fn execute<S: State + StateReader>(
         &self,
         state: &mut S,
         general_config: &StarknetGeneralConfig,
@@ -263,10 +263,7 @@ impl InternalInvokeFunction {
         )
     }
 
-    fn handle_nonce<S: State + StateReader + Clone>(
-        &self,
-        state: &mut S,
-    ) -> Result<(), TransactionError> {
+    fn handle_nonce<S: State + StateReader>(&self, state: &mut S) -> Result<(), TransactionError> {
         if self.version == 0 {
             return Ok(());
         }
